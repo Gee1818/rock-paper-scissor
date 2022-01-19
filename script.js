27065971257206846259eb2d983f3f7cd7eaac21
Gee <g1sw@hotmail.com>
2022-01-18 22:36:10 -0300

    First revision of game

---
+++ script.js
@@ -0,0 +1,40 @@
+// Random computer selection
+function computerMove(){
+    // let options = ['rock','paper','scissor'];
+    // return options[Math.floor(Math.random()*options.length)]
+    return 'scissor'
+}
+// Player selection
+function playerMove(){
+    //let pselect = 'Rock';
+    let pselect = window.prompt("Enter your move    : ")
+    pselect = pselect.toLowerCase();
+
+    return pselect;
+}
+// Game execution
+function playRound(computerSelection,playerSelection){
+    let result = 'asasd';
+    if (computerSelection === playerSelection) {
+        result = 'Draw';
+    }
+    if ((computerSelection === 'rock' && playerSelection === 'scissor') || 
+    (computerSelection === 'scissor' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'rock')) {
+        result = 'ComputerWins'
+    }
+    if ((playerSelection === 'rock' && computerSelection === 'scissor') || 
+    (playerSelection === 'scissor' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')) {
+        result = 'PlayerWins'
+    }
+    
+    return result;
+
+}
+
+
+
+// console.log(playerMove());
+// console.log(computerMove());
+console.log(playRound(computerMove(),playerMove()));
