3f74b90ffaac24b64c8504ee804240db8ac4bd71
Gee <g1sw@hotmail.com>
2022-02-13 16:11:35 -0300

    Added functionality

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+//variables
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const playermove = document.getElementById('playermove');
+const computermove = document.getElementById('computermove');
+const pCounter = document.getElementById('pCounter')
+const cCounter = document.getElementById('cCounter')
+let pscore=0;
+let cscore=0;
+
+//button clicks
+rock.addEventListener('click', () => handleclick('rock'))
+paper.addEventListener('click', () => handleclick('paper'))
+scissors.addEventListener('click', () => handleclick('scissors'))
+
 // Random computer selection
 function computerMove(){
     let options = ['rock','paper','scissor'];
     return options[Math.floor(Math.random()*options.length)]
-    //return 'scissor'
 }
-// Player selection
-function playerMove(){
-    //let pselect = 'Rock';
-    let pselect = window.prompt("Enter your move: ")
-    pselect = pselect.toLowerCase();
 
-    return pselect;
-}
 // Game execution
 function playRound(computerSelection,playerSelection){
     let result = 'asasd';
-    console.log(playerSelection);
-    console.log(computerSelection);
     
     if (computerSelection === playerSelection) {
         result = 'Draw';
@@ -25,19 +31,34 @@ function playRound(computerSelection,playerSelection){
     (computerSelection === 'scissor' && playerSelection === 'paper') ||
     (computerSelection === 'paper' && playerSelection === 'rock')) {
         result = 'ComputerWins'
+        cscore++
     }
     if ((playerSelection === 'rock' && computerSelection === 'scissor') || 
     (playerSelection === 'scissor' && computerSelection === 'paper') ||
     (playerSelection === 'paper' && computerSelection === 'rock')) {
         result = 'PlayerWins'
+        pscore++
     }
     
-    return result;
+    updateScore(result)
 
 }
 
+//Score update
+function updateScore(winner){
+    if (winner === 'ComputerWins'){
+        cCounter.textContent = cscore
+    }
+    if (winner === 'PlayerWins'){
+        pCounter.textContent = pscore
+    }
+}
 
 
-// console.log(playerMove());
-// console.log(computerMove());
-console.log(playRound(computerMove(),playerMove()));
+function handleclick(playerSelection){
+    let computerSelection = computerMove();
+    playRound(computerSelection,playerSelection);
+
+    playermove.textContent = playerSelection;
+    computermove.textContent = computerSelection;
+}
